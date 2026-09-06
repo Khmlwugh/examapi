2e95eb03422656d1e605f0e9132df572b82a06f4
Gabriel Flores <gaabf@hotmail.com>
2026-09-05 20:19:10 -0500

    ADDED: Initial commit of template project struture from Spring Initializr

---
+++ src/main/java/dev/heygabo/examapi/ExamapiApplication.java
@@ -0,0 +1,13 @@
+package dev.heygabo.examapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/heygabo/examapi/ExamapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.heygabo.examapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
